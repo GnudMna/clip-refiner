bc8a47530cbd950d1cae6d406c9c7fceefcea718
GnudMna <gnudlemna.4656@gmail.com>
2026-02-08 03:42:59 +0900

    Chore: パッケージ名変更に伴うビルドロジックの変更

--- build.rs
+++ build.rs
@@ -2,11 +2,13 @@ fn main() {
     // Windowsの場合はexeに情報を追加
     #[cfg(windows)]
     {
+        const APP_NAME: &str = "ClipRefiner";
+
         let mut res = winres::WindowsResource::new();
-        
+
         // アプリ情報
-        res.set("ProductName", env!("CARGO_PKG_NAME"));
-        res.set("FileDescription", env!("CARGO_PKG_NAME"));
+        res.set("ProductName", APP_NAME);
+        res.set("FileDescription", APP_NAME);
 
         // バージョン情報
         res.set("FileVersion", env!("CARGO_PKG_VERSION"));
@@ -14,7 +16,7 @@ fn main() {
 
         // 著作権
         res.set("LegalCopyright", env!("CARGO_PKG_AUTHORS"));
-        
+
         // アイコン設定
         res.set_icon("assets/icon.ico");
 
